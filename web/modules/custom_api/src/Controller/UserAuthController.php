7a9f93996c4f65f490b95dc870efecdd8c337e1d
Gursimran07316 <gursimrankhela@gmail.com>
2025-05-19 22:23:34 -0400

    Changed jwt props

--- web/modules/custom_api/src/Controller/UserAuthController.php
+++ web/modules/custom_api/src/Controller/UserAuthController.php
@@ -56,7 +56,7 @@ class UserAuthController extends ControllerBase {
     ]);
 
     $response->headers->setCookie(new Cookie(
-      'STYXKEY-jwt', $jwt, time() + 30 * 24 * 60 * 60, '/', null, true, true, false, 'None'
+      'jwt', $jwt, time() + 30 * 24 * 60 * 60, '/', null, true, true, false, 'None'
     ));
 
     return $response;
@@ -64,7 +64,7 @@ class UserAuthController extends ControllerBase {
 
   public function logoutUser(Request $request) {
     $response = new JsonResponse(['message' => 'Logged out successfully.'], 200);
-    $response->headers->clearCookie('STYXKEY-jwt', '/', null, false, true, false, 'None');
+    $response->headers->clearCookie('jwt', '/', null, false, true, false, 'None');
     return $response;
   }
 
@@ -112,7 +112,7 @@ class UserAuthController extends ControllerBase {
     ], 201);
 
     $response->headers->setCookie(new Cookie(
-      'STYXKEY-jwt', $jwt, time() + 30 * 24 * 60 * 60, '/', null, true, true, false, 'None'
+      'jwt', $jwt, time() + 30 * 24 * 60 * 60, '/', null, true, true, false, 'None'
     ));
 
     return $response;
@@ -235,7 +235,7 @@ class UserAuthController extends ControllerBase {
 
   private function getAuthenticatedUser(bool $requireAdmin = false): User {
     $request = \Drupal::request();
-    $jwt = $request->cookies->get('STYXKEY-jwt');
+    $jwt = $request->cookies->get('jwt');
 
     if (!$jwt) {
       throw new \Exception('Not authorized, no token.', 401);
